444b8384455ab7cd49495be9decb4f66bcedbb3b
Hamdi Ismail <hamdihismail@gmail.com>
2023-03-08 09:09:43 -0500

    added instructions to project overview

--- src/components/ProjectOverview.js
+++ src/components/ProjectOverview.js
@@ -23,15 +23,30 @@ const ProjectOverview = () => {
           HTML, CSS, JavaScript, Node.js, and APIs. The site is responsive and
           user-friendly, with seamless integration of third-party services.
         </p>
-        <h2>features</h2>
-        <ul>
-          <li>User authentication using Auth0</li>
-          <li>Secure payment processing with Stripe</li>
-          <li>Airtable integration for efficient data storage</li>
-          <li>Filter and sort functionality</li>
-          <li>Seamless integration of third-party services</li>
-          <li>Responsive design for optimal viewing on all devices</li>
-        </ul>
+        <div className='container'>
+          <div className='features-container'>
+            <h2>features</h2>
+            <ul>
+              <li>User authentication using Auth0</li>
+              <li>Secure payment processing with Stripe</li>
+              <li>Airtable integration for efficient data storage</li>
+              <li>Filter and sort functionality</li>
+              <li>Seamless integration of third-party services</li>
+              <li>Responsive design for optimal viewing on all devices</li>
+            </ul>
+          </div>
+          <div className='instructions-container'>
+            <h2>instructions</h2>
+            <ul>
+              <li>Login using username "user" and password "user"</li>
+              <li>Add items to the cart</li>
+              <li>Proceed to checkout</li>
+              <li>Use the provided test card for payment</li>
+              <li>Enter any future date for the expiry date</li>
+              <li>Enter any 5 digits for the zip code</li>
+            </ul>
+          </div>
+        </div>
         <h2>Technologies Used</h2>
         <ul>
           <li>React</li>
@@ -95,5 +110,12 @@ const OverviewContainer = styled.aside`
     color: var(--clr-white);
     list-style-type: disc;
     margin-bottom: 2rem;
+  }
+  .container {
+    display: flex;
+    gap: 2rem;
+    @media screen and (max-width: 800px) {
+      flex-direction: column;
+    }
   }
 `;
